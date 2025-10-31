58c08a01acacd8554720cc84a85b762673cdd515
Cream <cream@MacBookAir.ST-244F>
2025-10-31 11:59:52 +0700

    update register

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -5,11 +5,57 @@ class RegisterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+
+    final TextEditingController emailController = TextEditingController();
+
+    return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
-          child: Text("register page"),
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Sign up",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 40,),
+
+
+                TextField(
+                 controller: emailController,
+                 decoration: InputDecoration(
+                    labelText: "Email",
+                    prefixIcon: const Icon(Icons.email),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                 ),
+                 keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 20,),
+
+                TextField(
+                 controller: emailController,
+                 decoration: InputDecoration(
+                    labelText: "Name",
+                    prefixIcon: const Icon(Icons.person_pin_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                 ),
+                 keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 20,)
+
+              ],
+            ),
+          ),
         ),
       ),
     );
